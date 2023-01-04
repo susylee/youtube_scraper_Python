78ae8e7a1a2cf984fa3b5f9cd00d5a78cbe31ef2
Suyeon Lee <lsy7674@gmail.com>
2023-01-04 17:34:15 -0500

    created new routes for redirect

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template, redirect
 from app import app
+from app.forms import SearchForm
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    return "Hola chicos!!!!"
+    search_form = SearchForm()
+    if search_form.validate_on_submit():
+        return redirect('/results')
+    print(search_form.errors    )
+    return render_template('index.html', form=search_form)
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
+
+
