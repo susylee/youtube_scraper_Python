938f5cbd6138c9d9db44d73381871b51842d4af8
Suyeon Lee <lsy7674@gmail.com>
2023-01-04 17:29:03 -0500

    created the search form

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+
+class SearchForm(FlaskForm):
+    query= StringField("Youtube Channel", validators=[DataRequired()])
+    submit=SubmitField("Search")
