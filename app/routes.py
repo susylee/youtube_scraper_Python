9a84f01db0d521a6d2e516f19a9eb2b41481b9a0
Suyeon Lee <lsy7674@gmail.com>
2023-01-10 18:15:23 -0500

    app/routes.py working

--- app/routes.py
+++ app/routes.py
@@ -7,44 +7,55 @@ from selenium import webdriver
 import re, json, time
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
+import requests
+from bs4 import BeautifulSoup
+import re
+import json
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
     search_form = SearchForm()
     if search_form.validate_on_submit():
-        driver = webdriver.Chrome()
-        driver.get(f'https://www.youtube.com/results?search_query={search_form.data["query"]}')
+        # driver = webdriver.Chrome()
+        # driver.get(f'https://www.youtube.com/results?search_query={search_form.data["query"]}')
 
-        try:
-            channel_section=driver.find_element(By.TAG_NAME,'ytd-channel-renderer')
-        except NoSuchElementException:
-            channel_section=None
+        # try:
+        #     channel_section=driver.find_element(By.TAG_NAME,'ytd-channel-renderer')
+        # except NoSuchElementException:
+        #     channel_section=None
         
-        if not channel_section:
-            return redirect(url_for('notfound'))
+        # if not channel_section:
+        #     return redirect(url_for('notfound'))
 
-        print("found it")
-        youtube_data = []
+        # print("found it")
+        # youtube_data = []
 
-        # response = requests.get(f'https://www.youtube.com/results?search_query={search_form.data["query"]}')
-        # # print(response) #200
-        # soup = BeautifulSoup(response.content, 'html.parser')
-        # # We locate the JSON data using a regular-expression pattern
-        # data = re.search(r"var ytInitialData = ({.*});", str(soup)).group(1)
-        # # json_data = json.loads(data)
+        headers={
+        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        }
+        response = requests.get(f'https://www.youtube.com/results?search_query={search_form.data["query"]}', headers=headers)
+        #print(response) #200
+        time.sleep(3)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        aid=soup.find('script',string=re.compile('ytInitialData'))
+        extracted_josn_text=aid.text.split(';')[0].replace('window["ytInitialData"] =','').strip()
+        print(extracted_josn_text)
+        # video_results=json.loads(extracted_josn_text)
+        # #print(item_section=video_results["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][1])
+        # item_section=video_results["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]
 
-        # with open('readme.txt', 'w') as f:
-        #     f.write(data)
-        
-        # # print(json_data)
-        # # Uncomment to view all the data
-        # # print(json.dumps(data))
-        # # search = soup.select("#search")
-        # # print(soup.prettify())
-        # # print(soup.find('#contents'))
-        # print(search_form.errors)
+        # for item in item_section:
+        #     try:
+        #         video_info=item["videoRenderer"]
+        #         title=video_info["title"]["simpleText"]
+        #         url=video_info["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"]["url"]
+        #         print('Title:',title)
+        #         print('Url:',url, end="\n----------\n")
+        #     except KeyError:
+        #             pass
+
+
 
         return redirect(url_for('results', keyword=search_form.data['query']))
     return render_template('index.html', form=search_form)
